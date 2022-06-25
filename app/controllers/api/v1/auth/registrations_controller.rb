c36067a27d3b4539ff91043a72d651c0daf9d8bf
DeargGlen <tam.fse3982@gmail.com>
2022-06-25 05:07:51 +0000

    change for production

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
-  # before_action :authenticate_api_v1_user!
+  before_action :authenticate_api_v1_user!
   private
 
     def sign_up_params
